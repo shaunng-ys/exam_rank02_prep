c635f9e5ae637314c36b8ccbef7efd015168a0dc
Shaun Ng <shaunng_ys@proton.me>
2025-04-19 22:26:10 +0800

    Added completed level one exercises - snake_to_camel.c

---
+++ level1/snake_to_camel.c
@@ -0,0 +1,49 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int	main(int argc, char **argv)
+{
+	size_t	i;
+	size_t	trigger;
+
+	i = 0;
+	trigger = 0;
+	if (argc == 2)
+	{
+		while (argv[1][i])
+		{
+			while(argv[1][i] != '_' && trigger == 0 && argv[1][i])
+			{
+				ft_putchar(argv[1][i]);
+				i++;
+			}
+			if (argv[1][i] == '_' && trigger == 0 && argv[1][i + 1])
+			{
+				ft_putchar(argv[1][i + 1] - 32);
+				i = i + 2;
+				trigger = 1;
+			}
+			if (argv[1][i] == '_' && argv[1][i] && trigger == 1)
+			{
+				i++;
+				ft_putchar(argv[1][i] - 32);
+				i++;
+			}
+			while(argv[1][i] && argv[1][i] != '_')
+			{
+				ft_putchar(argv[1][i]);
+				i++;
+			}
+		}
+		ft_putchar('\n');
+	}
+	else
+		ft_putchar('\n');
+	return (0);
+}
